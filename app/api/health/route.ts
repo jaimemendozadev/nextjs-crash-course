98f6f89b66fa1238d68bf176a34acbb6804df0dd
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-09-20 19:57:35 -0700

    Add DELETE API callback example

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,4 +1,9 @@
-/*
+// @ts-nocheck
+
+/****************************************** 
+ *  GET API Callback
+ ****************************************** 
+
 
 import { NextResponse } from 'next/server';
 
@@ -9,6 +14,50 @@ export async function GET() {
 }
 
 */
+
+
+
+
+/*****************************************************
+ *  DELETE API Callback with await params example
+ ***************************************************** 
+
+
+import { NextResponse } from 'next/server';
+
+
+export async function DELETE(
+  request,
+  { params }
+) {
+
+
+  try {
+    const { fileID } = await params;
+
+
+    return NextResponse.json(
+        { status: 200, message: 'Your message was successfully deleted.' },
+        { status: 200 }
+      );
+
+
+  } catch (error) {
+
+    console.log(`Error in DELETE callback `, error);
+
+    return NextResponse.json(
+      {
+        status: 500,
+        message: `There was a problem deleting the resource. Try again later.`
+      },
+      { status: 500 }
+    );
+  }
+}
+
+
+
 
 
 // Next.js API Routes Docs: https://nextjs.org/docs/app/guides/migrating/app-router-migration#api-routes
