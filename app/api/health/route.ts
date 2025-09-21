38c918e60c632c345d7a35c7607f0146e02b8a46
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-09-20 19:20:47 -0700

    Scaffold /api/health route

---
+++ app/api/health/route.ts
@@ -0,0 +1,14 @@
+/*
+
+import { NextResponse } from 'next/server';
+
+
+export async function GET() {
+  const data = { message: "I'm ok. 👍🏽" };
+  return NextResponse.json(data, { status: 200 });
+}
+
+*/
+
+
+// Next.js API Routes Docs: https://nextjs.org/docs/app/guides/migrating/app-router-migration#api-routes
