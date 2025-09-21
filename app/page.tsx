33072a918c27366ced68a6eccc14fa1f0a5fca62
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-09-20 18:55:05 -0700

    Un-nuked TS Next.js app and reverted changes
    Creating a Vanilla React.js app in a Next.js
    configured app sans TS could result in breaking
    changes. Decided not to confuse students with
    manually configuring Next.js app to NOT USE TS.

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { Banner } from "@/components/banner";
+
+export default function Home() {
+  return (
+    <Banner />
+  );
+}
---
+++ components/banner.tsx
@@ -0,0 +1,5 @@
+
+
+export function Banner() {
+    return <h1>👋🏼 Welcome to DogDate! Woof Woof 🐶</h1>
+}
